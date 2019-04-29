a93ff3d6e8a0ff030b23eeaf8cec3793efb9a704
JarlPenguin <jarlpenguin@outlook.com>
2019-04-29 17:13:09 +0300

    cactus: inherit vendor makefile
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -1,2 +1,5 @@
 # Inherit common device config
 $(call inherit-product, device/xiaomi/mt6765-common/mt6765.mk)
+
+# Inherit vendor makefile
+$(call inherit-product, vendor/xiaomi/cactus/cactus-vendor.mk
