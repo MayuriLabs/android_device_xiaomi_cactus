56d38e16e2a54d68b067b1c8c80ee9b72f1d9865
JarlPenguin <jarlpenguin@outlook.com>
2019-05-01 10:05:08 +0300

    cactus: import moar configs
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -2,7 +2,8 @@
 $(call inherit-product, device/xiaomi/mt6765-common/mt6765.mk)
 
 # Inherit vendor makefile
-$(call inherit-product, vendor/xiaomi/cactus/cactus-vendor.mk
+$(call inherit-product-if-exists, vendor/xiaomi/mt6765-common/mt6765-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/cactus/cactus-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES +=  \
@@ -16,13 +17,23 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
+PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
     $(LOCAL_PATH)/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
     $(LOCAL_PATH)/bluetooth/mtk_bt_fw.conf:system/etc/bluetooth/mtk_bt_fw.conf \
     $(LOCAL_PATH)/bluetooth/mtk_bt_stack.conf:system/etc/bluetooth/mtk_bt_stack.conf \
     $(LOCAL_PATH)/bluetooth/wt_bt_stack.conf:system/etc/bluetooth/wt_bt_stack.conf
     
+# Public Libraries
+PRODUCT_COPY_FILES +=  \
+    $(LOCAL_PATH)/configs/public.libraries.txt:system/etc/public.libraries.txt
+
 # Priv-app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-mediatek.xml:system/etc/privapp-permissions-mediatek.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-platform.xml:system/etc/privapp-permissions-platform.xml
+
+# Seccomp Policy
+PRODUCT_COPY_FILES +=  \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/etc/seccomp_policy/mediaextractor.policy
