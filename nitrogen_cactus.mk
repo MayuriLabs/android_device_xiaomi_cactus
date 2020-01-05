986c4bc02ddede50fbf8fddb7a7c76a338721e3d
wulan17 <galihgustip@gmail.com>
2020-01-05 14:14:59 +0700

    cactus: Nitrogen-ify
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- nitrogen_cactus.mk
+++ nitrogen_cactus.mk
@@ -17,12 +17,12 @@
 # Inherit from device
 $(call inherit-product, device/xiaomi/cactus/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cactus
-PRODUCT_NAME := lineage_cactus
+PRODUCT_NAME := nitrogen_cactus
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6A
 PRODUCT_MANUFACTURER := Xiaomi
