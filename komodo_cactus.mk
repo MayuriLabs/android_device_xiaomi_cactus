e09aa28cef16769b9e177304f537575ec733eb4f
wulan17 <galihgustip@gmail.com>
2020-09-16 22:10:29 +0700

    cactus: set BUILD_VARIANT to BETA
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- komodo_cactus.mk
+++ komodo_cactus.mk
@@ -20,7 +20,7 @@ $(call inherit-product, device/xiaomi/cactus/device.mk)
 # Inherit some common Komodo OS stuff.
 $(call inherit-product, vendor/komodo/config/common_full_phone.mk)
 CURRENT_BUILD_TYPE := nogapps
-KOMODO_VARIANT := RELEASE
+KOMODO_VARIANT := BETA
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cactus
