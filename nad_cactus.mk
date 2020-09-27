3f277a09e4747ec6d604007e50e3afa10f33f76a
wulan17 <galihgustip@gmail.com>
2020-09-27 21:09:20 +0700

    cactus: add nad Official tag
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- nad_cactus.mk
+++ nad_cactus.mk
@@ -20,6 +20,9 @@ $(call inherit-product, device/xiaomi/cactus/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
+# Official tag
+NAD_BUILD_TYPE := OFFICIAL
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cactus
 PRODUCT_NAME := nad_cactus
