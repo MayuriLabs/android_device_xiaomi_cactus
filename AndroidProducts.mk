dc7174209d3079fa4017ed5b4dfa1f4a6bac37b4
wulan17 <galihgustip@gmail.com>
2020-09-27 21:06:02 +0700

    cactus: Nusantara-ROM Bringup!
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cactus.mk
+    $(LOCAL_DIR)/nad_cactus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cactus-user \
-    lineage_cactus-userdebug \
-    lineage_cactus-eng
+    nad_cactus-user \
+    nad_cactus-userdebug \
+    nad_cactus-eng
